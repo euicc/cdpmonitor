26f985dae4db632a9fb5623f18a0dde26671b58d
eumb <eumb@hotmail.com>
2018-11-01 10:38:17 -0400

    tocsv

--- Common/ping.py
+++ Common/ping.py
@@ -1,56 +1,31 @@
 import subprocess
 import os
-import json
-from easydict import EasyDict as edict
+import csv
 import time
 import datetime
 
 ts = time.time()
 
 
-d = edict({'tests':{'testName':'pingtest','result':{}}})
-d.tests.timestamp = datetime.datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%d %H:%M:%S')
+timestamp = datetime.datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%d %H:%M:%S')
 
 
-filename = "data.json"
+filename = "results/result.csv"
 
-
-#print("Reading %s" % filename)
-#try:
-#    with open(filename, "r") as fp:
-#        d = json.load(fp)
-#    print(d)
-#except IOError:
-#    print("Could not read file, starting from scratch")
-
-#runtime = datetime.datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%d %H:%M:%S')
-
-
-
-#d["tests"].append(dict(timestamp=runtime))
 
 with open('servers.txt', 'r') as f:
-        for ip in f:
+      lines = f.readlines()
+      lines = [line.rstrip('\n') for line in open('servers.txt')]   
+      for ip in lines:
             result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2",    ip],stdout=f, stderr=f).wait()
             if result:
                 print(ip, "inactive")
-		d.tests.result[ip] = {
-                   "result": {
-                             "name": "ping",
-                             "ip":ip,
-                             "result":result
-                              }
-            	}
-		with open(filename, "w") as fp:
-    			json.dump(d, fp,indent=4)
+		with open(filename, 'a') as csvfile:
+    			resultwriter = csv.writer(csvfile, delimiter=',')
+    			resultwriter.writerow(["ping",timestamp,ip,result])
             else:
                 print(ip, "active")
-		d.tests.result[ip] = {
-                   "result": {
-                             "name": "ping",
-                             "ip":ip,
-                             "result":result
-                              }
-           	}
-		with open(filename, "w") as fp:
-    			json.dump(d, fp,indent = 4)
+		with open(filename, 'a') as csvfile:
+                	resultwriter = csv.writer(csvfile, delimiter=',')
+                	resultwriter.writerow(["ping",timestamp,ip,result])
+
